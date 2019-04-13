4b9c1fcecb00905e5cb2d789379f42547f141f0c
shreyas271198 <shreyas271198gmail.com>
2019-04-13 18:19:52 +0530

    version2 elimination lone ranger

--- pr1.cpp
+++ pr1.cpp
@@ -17,7 +17,32 @@ void printGrid(int grid[N][N])
 
 int main(){
 int count=1,old_count=0;
-int grid[N][N] =     {{0,15,0,1,0,2,10,14,12,0,0,0,0,0,0,0},
+/*int grid[N][N] =     {{10,22,14,0,0,0,0,13,0,18,0,20,25,0,0,0,21,0,1,0,0,0,0,12,0 },
+{0,0,0,12,0,17,15,1,25,10,0,3,13,0,7,0,0,16,0,0,11,18,24,4,0 },
+{0,0,0,0,0,0,0,0,0,0,24,12,0,0,0,0,0,0,2,0,10,25,6,1,0 },
+{0,0,0,9,0,0,0,23,0,5,0,0,19,0,0,0,0,12,0,0,17,0,20,0,0 },
+{0,0,5,0,16,12,6,20,8,0,0,0,0,0,0,10,0,0,0,0,0,2,19,0,0 },
+{5,0,0,20,0,0,0,9,14,6,0,0,0,0,17,1,0,0,0,0,0,23,16,11,0 },
+{24,0,16,0,0,0,0,19,15,0,0,5,18,1,0,8,0,25,6,0,0,0,12,0,0 },
+{0,0,12,14,0,0,0,0,17,0,0,0,0,24,20,13,0,0,3,0,0,0,0,0,4 },
+{0,18,19,13,0,0,0,10,0,3,2,0,9,6,0,16,0,23,0,21,0,0,0,24,0 },
+{0,0,1,6,9,0,24,0,0,12,0,16,22,0,0,20,19,0,17,0,0,0,2,0,0 },
+{0,0,0,0,0,0,0,21,0,0,0,24,0,3,13,18,11,17,0,0,0,0,22,0,1 },
+{0,0,0,0,0,0,0,0,0,9,0,0,8,0,1,0,0,0,0,0,0,24,0,7,0 },
+{0,0,0,0,12,18,0,0,0,0,0,0,0,14,2,21,8,0,13,1,19,0,9,0,0 },
+{0,14,0,0,0,15,3,4,7,0,5,0,0,0,0,0,6,0,0,22,8,0,21,0,2 },
+{0,16,3,0,25,0,0,0,0,1,0,0,12,9,21,0,2,15,7,20,0,0,0,0,0 },
+{0,0,0,0,0,23,0,0,0,15,0,0,0,16,0,7,14,0,0,0,3,0,13,0,0 },
+{0,0,0,4,7,9,0,14,0,0,0,1,0,8,0,0,0,0,24,0,6,22,0,0,0 },
+{0,5,0,1,6,0,0,12,11,0,0,25,14,0,0,0,0,13,0,19,24,0,0,0,0 },
+{0,9,0,0,0,4,13,0,0,8,19,0,0,7,0,0,0,21,20,0,25,1,0,0,11 },
+{16,0,11,0,0,0,20,3,0,25,0,23,0,0,9,22,1,6,15,0,4,7,14,0,0 },
+{0,0,0,16,0,3,0,0,0,0,0,0,4,0,0,6,12,8,0,13,0,0,0,0,0 },
+{14,20,0,7,22,0,16,5,12,0,0,0,21,23,25,4,0,1,0,0,0,0,3,0,0 },
+{0,1,0,15,0,20,0,0,4,0,8,0,0,0,0,0,0,0,0,16,14,12,17,19,24 },
+{0,0,0,25,0,0,0,0,0,7,0,0,3,0,22,14,0,0,0,0,0,0,10,0,0 },
+{3,0,0,0,5,14,25,15,18,13,0,7,0,17,0,0,20,0,22,0,0,11,0,0,0 }};*/
+ int grid[N][N] = {{0,15,0,1,0,2,10,14,12,0,0,0,0,0,0,0},
                       {0,6,3,16,12,0,8,4,14,15,1,0,2,0,0,0},
                       {14,0,9,7,11,3,15,0,0,0,0,0,0,0,0,0},
                       {4,13,2,12,0,0,0,0,6,0,0,0,0,15,0,0},
@@ -37,10 +62,10 @@ int grid[N][N] =     {{0,15,0,1,0,2,10,14,12,0,0,0,0,0,0,0},
 
 //vector<vector<vector<bool>>> po(N,vector<vector<bool>>(N,vector<bool>(N,1)));
 int po[N][N][N];
-for(int r=0;r<N;r++){
+for(int r=0;r<N;r++){                         //initialization of posibility matrix 
 	for(int c=0;c<N;c++){
 		for(int d=0;d<N;d++){
-			po[r][c][d]=1;
+			po[r][c][d]=1;                      //posibility = 1 ; not possible =0;
 		}
 	}
 }
@@ -50,7 +75,7 @@ for(int r=0;r<N;r++){
 			for(int k=0;k<N;k++){
 				po[r][c][k]=0;
 			}
-		po[r][c][grid[r][c]-1]=1;	
+		po[r][c][grid[r][c]-1]=1;                 //posibility of existing grid	
 		}
 	}
 }
@@ -58,7 +83,7 @@ for(int r=0;r<N;r++){
 while(old_count!=count){
 old_count=count;
 count=0;
-for(int i=0;i<N;i++){               //row and column check
+for(int i=0;i<N;i++){               //row and column check 
 	vector<int> rv;
 	vector<int> cv;
 	for(int j=0;j<N;j++){
@@ -69,7 +94,7 @@ for(int i=0;i<N;i++){               //row and column check
 			cv.push_back(grid[j][i]-1);
 		}
 	}
-	for(int j=0;j<N;j++){
+	for(int j=0;j<N;j++){                    //removing checked numbers from posibility matrix 
 		if(grid[i][j]==0){
 			for(vector<int>::iterator k=rv.begin();k!=rv.end();k++){
 			po[i][j][*k]=0;}
@@ -96,7 +121,7 @@ for(int br=0;br<N;br+=n){                   //box check
 		for(int i=0;i<n;i++){
 		for(int j=0;j<n;j++){
 			for(vector<int>::iterator k=bv.begin();k!=bv.end();k++){
-			if(grid[br+i][bc+j]==0){
+			if(grid[br+i][bc+j]==0){                //removing from posibility matrix
 			po[br+i][bc+j][*k]=0;
 			}
 			}
@@ -106,47 +131,72 @@ for(int br=0;br<N;br+=n){                   //box check
 	}
 }	
 
-for(int r=0;r<N;r++){                 //elemination
+for(int r=0;r<N;r++){                 //elemination row-wise  
 	for(int c=0;c<N;c++){
 		int a1=0,flg=0;
-		for(int k=0;k<N;k++){
+		for(int k=0;k<N;k++){          //checking each posibility cell in number direction
+		if(grid[r][c]==k+1){continue;}           
 			a1+=po[r][c][k];
 			flg+=k*po[r][c][k];
 		}
 		if(a1==1){
 			count++;
 			grid[r][c]=flg+1;
+
 		}
 	}
 }
-for(int k=0;k<N;k++){                 //long ranger
+
+for(int k=0;k<N;k++){                 //lone ranger
 for(int i=0;i<N;i++){
 	int a1=0,a2=0,flg1=0,flg2=0;
 	for(int j=0;j<N;j++){
-		a1+=po[i][j][k];
-		a2+=po[j][i][k];
+		a1+=po[i][j][k];              // kth number in row  
+		a2+=po[j][i][k];             //kth number in column
 		flg1+=j*po[i][j][k];
 		flg2+=j*po[j][i][k];
 	
 	}
-	if(a1==1){
+	if(a1==1 && grid[i][flg1]==0){
 		grid[i][flg1]=k+1;
-		for(int ex=0;ex<N;ex++){
+		for(int ex=0;ex<N;ex++){         //remove posibility after assignment
 			po[i][flg1][ex]=0;
 		}
 		po[i][flg1][k]=1;
 		count++;
+		
 	}
-	if(a2==1){
+	if(a2==1 && grid[flg2][i]==0){        //remove posibility after assignment
 		grid[flg2][i]=k+1;
 		for(int ex=0;ex<N;ex++){
 			po[flg2][i][ex]=0;
 		}
 		po[flg2][i][k]=1;
 		count++;
+		
 	}
 }
+for(int br=0;br<N;br+=n){                   //box check
+	for(int bc=0;bc<N;bc+=n){
+		
+		int a1=0,flg1=0,flg2=0;
+		for(int r=0;r<n;r++){
+			for(int c=0;c<n;c++){
+				a1+=po[br+r][bc+c][k];
+				flg1=r*po[br+r][bc+c][k];
+				flg2=c*po[br+r][bc+c][k];
+			}
+		}
+
+	}
 }
+for(int k=0;k<N;k++){
+for(int r=0;r<N;r++){
+	for int(c=0;c<N;c++){
+
+	}
+}
+} 
 }
 
 printGrid(grid); 
