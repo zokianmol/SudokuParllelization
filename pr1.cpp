9c954874cabe35ead4d180945ba61a83b11d77df
shreyas271198 <shreyas271198gmail.com>
2019-04-14 01:07:53 +0530

    version3 elimination lone ranger twin with functions

--- pr1.cpp
+++ pr1.cpp
@@ -5,15 +5,343 @@ using namespace std;
 #define N 16
 #define n 4
 
+void init(int grid[N][N],bool po[N][N][N]){
+	for(int r=0;r<N;r++){                         //initialization of posibility matrix 
+	for(int c=0;c<N;c++){
+		for(int d=0;d<N;d++){
+			po[r][c][d]=1;                      //posibility = 1 ; not possible =0;
+		}
+	}
+}
+for(int r=0;r<N;r++){
+	for(int c=0;c<N;c++){
+		if(grid[r][c]!=0){
+			for(int k=0;k<N;k++){
+				po[r][c][k]=0;
+			}
+		po[r][c][grid[r][c]-1]=1;                 //posibility of existing grid	
+		}
+	}
+}
+}
+
+void RemoveConstrain(int grid[N][N],bool po[N][N][N]){
+	for(int i=0;i<N;i++){               //row and column check 
+	vector<int> rv;
+	vector<int> cv;
+	for(int j=0;j<N;j++){
+		if(grid[i][j]!=0){
+			rv.push_back(grid[i][j]-1);
+		}
+		if(grid[j][i]!=0){
+			cv.push_back(grid[j][i]-1);
+		}
+	}
+	for(int j=0;j<N;j++){                    //removing checked numbers from posibility matrix 
+		if(grid[i][j]==0){
+			for(vector<int>::iterator k=rv.begin();k!=rv.end();k++){
+			po[i][j][*k]=0;}
+		}
+		if(grid[j][i]==0){
+			for(vector<int>::iterator k=cv.begin();k!=cv.end();k++){
+			po[j][i][*k]=0;}
+		}
+	}
+	rv.clear();
+	cv.clear();
+	}
+
+for(int br=0;br<N;br+=n){                   //box check
+	for(int bc=0;bc<N;bc+=n){
+		vector<int> bv;
+		for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			if(grid[br+i][bc+j]!=0){
+			bv.push_back(grid[br+i][bc+j]-1);
+			}
+		}
+		}
+		for(int i=0;i<n;i++){
+		for(int j=0;j<n;j++){
+			for(vector<int>::iterator k=bv.begin();k!=bv.end();k++){
+			if(grid[br+i][bc+j]==0){                //removing from posibility matrix
+			po[br+i][bc+j][*k]=0;
+			}
+			}
+			}
+		}
+	bv.clear();
+	}
+}	
+}
 void printGrid(int grid[N][N]) 
 { 
 	for (int row = 0; row < N; row++) 
 	{ 
 	for (int col = 0; col < N; col++) 
-			printf("%3d", grid[row][col]); 
+				printf("%3d",grid[row][col]); 
 		printf("\n"); 
 	} 
 } 
+
+bool elemination(int grid[N][N],bool po[N][N][N]){         //eleminitaio	n function
+	bool count=0;
+	for(int r=0;r<N;r++){                 //elemination row-wise  
+	for(int c=0;c<N;c++){
+		int a1=0,flg=0;
+		for(int k=0;k<N;k++){          //checking each posibility cell in number direction
+		if(grid[r][c]==k+1){continue;}           
+			a1+=po[r][c][k];
+			flg+=k*po[r][c][k];
+		}
+		if(a1==1){
+			count=1;
+			grid[r][c]=flg+1;
+
+		}
+	}
+}
+cout<<"closing elem"<<endl;
+return count;
+}
+
+bool loneR(int grid[N][N],bool po[N][N][N]){
+bool count=0;	
+for(int k=0;k<N;k++){                 //lone ranger
+for(int i=0;i<N;i++){
+	int a1=0,a2=0,flg1=0,flg2=0;
+	for(int j=0;j<N;j++){
+		a1+=po[i][j][k];              // kth number in row  
+		a2+=po[j][i][k];             //kth number in column
+		flg1+=j*po[i][j][k];
+		flg2+=j*po[j][i][k];
+	
+	}
+	if(a1==1 && grid[i][flg1]==0){
+		grid[i][flg1]=k+1;
+		for(int ex=0;ex<N;ex++){         //remove posibility after assignment for row
+			po[i][flg1][ex]=0;
+		}
+		po[i][flg1][k]=1;
+		count=1;
+		
+	}
+	if(a2==1 && grid[flg2][i]==0){        //remove posibility after assignment for column
+		grid[flg2][i]=k+1;
+		for(int ex=0;ex<N;ex++){
+			po[flg2][i][ex]=0;
+		}
+		po[flg2][i][k]=1;
+		count=1;
+		
+	}
+}
+
+for(int br=0;br<N;br+=n){                     //box lone ranger check         
+	for(int bc=0;bc<N;bc+=n){
+		
+		int a1=0,flg1=0,flg2=0;
+		for(int r=0;r<n;r++){
+			for(int c=0;c<n;c++){
+				a1+=po[br+r][bc+c][k];
+				flg1+=r*po[br+r][bc+c][k];
+				flg2+=c*po[br+r][bc+c][k];
+			}
+		}
+		if(a1==1 && grid[br+flg1][bc+flg2]==0){
+			count=1;
+			grid[br+flg1][bc+flg2]=k;
+			for(int r=0;r<n;r++){
+			for(int c=0;c<n;c++){
+				for(int num=0;num<N;num++){
+					po[br+flg1][bc+flg2][num]=0;
+				}
+				po[br+flg1][bc+flg2][k]=1;
+			}
+		}
+		}
+
+	}
+}
+cout<<"closing LR"<<endl;
+}
+return count;
+}
+
+bool twin(int grid[N][N],bool po[N][N][N]){
+bool count=0;	
+for(int r=0;r<N;r++){                             //find twin from row and column
+	vector<int> flg1,flg2,num1,num2;
+	for(int n1=0;n1<N;n1++){
+		int a1=0;
+		int b1=0;
+		for(int c=0;c<N;c++){
+			a1+=po[r][c][n1];
+			b1+=po[c][r][n1];
+		}
+		if(a1==2){
+		num1.push_back(n1);                    //find twin number in row
+		}
+    	if(b1==2){
+		num2.push_back(n1);                     //find twin number in column
+	    }
+	}
+	
+	if(num1.size()>=2){                        //if twin vector contain more than two
+	bool twin=1,brk=0;	                          //assume there is two twin numbers
+	for(vector<int>::iterator k=num1.begin();k!=num1.end()-1;k++){             //on first twin number =>k
+	for(vector<int>::iterator l=k+1;l!=num1.end();l++){                          //on second twin number =>l
+	for(int c=0;c<N;c++){
+		if(po[r][c][*k]!=po[r][c][*l]){                   //check posibility matrix rows of two twin numbers
+			twin=0;                                   //twin not found flag
+			break;
+		}
+	}
+	if(twin==1){                       //if twin found
+		for(int c=0;c<N;c++){                    
+			if(po[r][c][*k]==1){
+				flg1.push_back(c);                   //find location of twin 
+				cout<<"twin";
+				count=1;
+			}
+		}
+		for(int depth=0;depth<N;depth++){               //remove from posibility list
+			po[r][flg1[0]][depth]=0;
+			po[r][flg1[1]][depth]=0;
+
+		}
+		po[r][flg1[0]][*k]=1;                             //add single posibility
+		po[r][flg1[0]][*l]=1;
+		po[r][flg1[1]][*k]=1;
+		po[r][flg1[1]][*l]=1;
+		brk=1;
+		break;
+	}
+	else{
+		twin=1;
+	}
+
+	}
+	if(brk==1){
+		break;
+	}	
+	}
+	}
+
+	if(num2.size()>=2){                                 //colum twin find
+	bool twin=1,brk=0;	
+	for(vector<int>::iterator k=num2.begin();k!=num2.end()-1;k++)	{
+	for(vector<int>::iterator l=k+1;l!=num2.end();l++){
+	for(int c=0;c<N;c++){
+		if(po[c][r][*k]!=po[c][r][*l]){
+			twin=0;
+			break;
+		}
+	}
+	if(twin==1){
+		for(int c=0;c<N;c++){
+			if(po[c][r][*k]==1){
+				flg2.push_back(c);
+				cout<<"twin";
+				count=1;
+			}
+		}
+		for(int depth=0;depth<N;depth++){
+			po[flg2[0]][r][depth]=0;
+			po[flg2[1]][r][depth]=0;
+
+		}
+		po[flg2[0]][r][*k]=1;
+		po[flg2[0]][r][*l]=1;
+		po[flg2[1]][r][*k]=1;
+		po[flg2[1]][r][*l]=1;
+
+		brk=1;
+		break;
+	}
+	else{
+		twin=1;
+	}
+	
+	}
+	if(brk==1){
+		break;
+	}	
+	}
+	}
+    flg1.clear();
+    flg2.clear();
+    num1.clear();
+    num2.clear();
+}
+
+for(int br=0;br<N;br+=n){                     //box twin check         
+	for(int bc=0;bc<N;bc+=n){
+		vector<int> num1,flg1,flg2;
+		for(int n1=0;n1<N;n1++){
+		int a1=0;
+		for(int r=0;r<n;r++){
+			for(int c=0;c<n;c++){
+				a1+=po[br+r][bc+c][n1];
+			}
+		}
+		if(a1==2){
+			num1.push_back(n1);
+		}
+		}
+		if(num1.size()>=2){                        //if twin vector contain more than two
+		bool twin=1,brk=0;	                          //assume there is two twin numbers
+		for(vector<int>::iterator k=num1.begin();k!=num1.end()-1;k++){             //on first twin number =>k
+		for(vector<int>::iterator l=k+1;l!=num1.end();l++){
+			for(int r=0;r<n;r++){
+				for(int c=0;c<n;c++){
+					if(po[br+r][bc+c][*k]!=po[br+r][bc+c][*l]){
+						twin=0;
+						break;
+					}
+				}
+				if(twin==0){
+					break;
+				}
+			}
+			if(twin==1){
+				for(int r=0;r<n;r++){
+					for(int c=0;c<n;c++){
+						if(po[br+r][bc+c][*k]==1){
+							flg1.push_back(r);
+							flg2.push_back(c);
+							cout<<"twin"<<endl;
+							count=1;
+						}
+					}
+				}
+				for(int r=0;r<n;r++){
+					for(int c=0;c<n;c++){
+						po[br+r][bc+c][*k]=0;
+					}
+				}
+				po[br+flg1[0]][bc+flg2[0]][*k]=1;
+				po[br+flg1[0]][bc+flg2[0]][*l]=1;
+				po[br+flg1[1]][bc+flg2[1]][*k]=1;
+				po[br+flg1[1]][bc+flg2[1]][*l]=1;
+				brk=1;
+				break;
+			}
+			else{
+				twin=1;
+			}
+		}
+		if(brk==1){
+			break;
+		}
+		}
+		}
+	}
+}
+return count;
+}
+
+
 
 int main(){
 int count=1,old_count=0,itr=0;
@@ -62,214 +390,29 @@ int count=1,old_count=0,itr=0;
 
 //vector<vector<vector<bool>>> po(N,vector<vector<bool>>(N,vector<bool>(N,1)));
 bool po[N][N][N];
-for(int r=0;r<N;r++){                         //initialization of posibility matrix 
-	for(int c=0;c<N;c++){
-		for(int d=0;d<N;d++){
-			po[r][c][d]=1;                      //posibility = 1 ; not possible =0;
-		}
-	}
-}
-for(int r=0;r<N;r++){
-	for(int c=0;c<N;c++){
-		if(grid[r][c]!=0){
-			for(int k=0;k<N;k++){
-				po[r][c][k]=0;
-			}
-		po[r][c][grid[r][c]-1]=1;                 //posibility of existing grid	
-		}
-	}
-}
+init(grid,po);
 
 do{
 count=0;                            //start counting from each iteration
-for(int i=0;i<N;i++){               //row and column check 
-	vector<int> rv;
-	vector<int> cv;
-	for(int j=0;j<N;j++){
-		if(grid[i][j]!=0){
-			rv.push_back(grid[i][j]-1);
-		}
-		if(grid[j][i]!=0){
-			cv.push_back(grid[j][i]-1);
-		}
-	}
-	for(int j=0;j<N;j++){                    //removing checked numbers from posibility matrix 
-		if(grid[i][j]==0){
-			for(vector<int>::iterator k=rv.begin();k!=rv.end();k++){
-			po[i][j][*k]=0;}
-		}
-		if(grid[j][i]==0){
-			for(vector<int>::iterator k=cv.begin();k!=cv.end();k++){
-			po[j][i][*k]=0;}
-		}
-	}
-	rv.clear();
-	cv.clear();
-	}
-
-for(int br=0;br<N;br+=n){                   //box check
-	for(int bc=0;bc<N;bc+=n){
-		vector<int> bv;
-		for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++){
-			if(grid[br+i][bc+j]!=0){
-			bv.push_back(grid[br+i][bc+j]-1);
-			}
-		}
-		}
-		for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++){
-			for(vector<int>::iterator k=bv.begin();k!=bv.end();k++){
-			if(grid[br+i][bc+j]==0){                //removing from posibility matrix
-			po[br+i][bc+j][*k]=0;
-			}
-			}
-			}
-		}
-	bv.clear();
-	}
-}	
-
-for(int r=0;r<N;r++){                 //elemination row-wise  
-	for(int c=0;c<N;c++){
-		int a1=0,flg=0;
-		for(int k=0;k<N;k++){          //checking each posibility cell in number direction
-		if(grid[r][c]==k+1){continue;}           
-			a1+=po[r][c][k];
-			flg+=k*po[r][c][k];
-		}
-		if(a1==1){
-			count++;
-			grid[r][c]=flg+1;
-
-		}
-	}
-}
-
-for(int k=0;k<N;k++){                 //lone ranger
-for(int i=0;i<N;i++){
-	int a1=0,a2=0,flg1=0,flg2=0;
-	for(int j=0;j<N;j++){
-		a1+=po[i][j][k];              // kth number in row  
-		a2+=po[j][i][k];             //kth number in column
-		flg1+=j*po[i][j][k];
-		flg2+=j*po[j][i][k];
-	
-	}
-	if(a1==1 && grid[i][flg1]==0){
-		grid[i][flg1]=k+1;
-		for(int ex=0;ex<N;ex++){         //remove posibility after assignment for row
-			po[i][flg1][ex]=0;
-		}
-		po[i][flg1][k]=1;
-		count++;
-		
-	}
-	if(a2==1 && grid[flg2][i]==0){        //remove posibility after assignment for column
-		grid[flg2][i]=k+1;
-		for(int ex=0;ex<N;ex++){
-			po[flg2][i][ex]=0;
-		}
-		po[flg2][i][k]=1;
-		count++;
-		
-	}
-}
-
-for(int br=0;br<N;br+=n){                     //box lone ranger check         
-	for(int bc=0;bc<N;bc+=n){
-		
-		int a1=0,flg1=0,flg2=0;
-		for(int r=0;r<n;r++){
-			for(int c=0;c<n;c++){
-				a1+=po[br+r][bc+c][k];
-				flg1+=r*po[br+r][bc+c][k];
-				flg2+=c*po[br+r][bc+c][k];
-			}
-		}
-		if(a1==1 && grid[br+flg1][bc+flg2]==0){
-			count++;
-			grid[br+flg1][bc+flg2]=k;
-			for(int r=0;r<n;r++){
-			for(int c=0;c<n;c++){
-				for(int num=0;num<N;num++){
-					po[br+flg1][bc+flg2][num]=0;
-				}
-				po[br+flg1][bc+flg2][k]=1;
-			}
-		}
-		}
-
-	}
-}
-}
+RemoveConstrain(grid,po);
+count+=elemination(grid,po);         //return 1 if it is done
+count+=loneR(grid,po);
+count+=twin(grid,po);
 
 
-for(int r=0;r<N;r++){                             //find twin from row and column
-	vector<int> flg1,flg2,num1,num2;
-	for(int n1=0;n1<N;n1++){
-		int a1=0;
-		int b1=0;
-		for(int c=0;c<N;c++){
-			a1+=po[r][c][n1];
-			b1+=po[c][r][n1];
-		}
-		if(a1==2){
-		for(int k=0;k<N-1;k++){
-			if(po[r][k][n1]==1){
-				flg1.push_back(k);
-			}
-		}
-		num1.push_back(n1);
-		}
-    	if(b1==2){
-		for(int k=0;k<N-1;k++){
-			if(po[k][r][n1]==1){
-				flg2.push_back(k);
-	    	}
-		}
-		num2.push_back(n1);
-	    }
-	}
-	
-	if(flg1.size()==4){
-	if(flg1[0]==flg1[2] && flg1[1]==flg1[3]){
-	cout<<"twin"<<endl;	
-	for(vector<int>::iterator k=flg1.begin();k!=flg1.end();k++)	{
-	for(int i=0;i<N;i++){
-		po[r][*k][i]=0;
-	}
-    }
-	for(vector<int>::iterator k=flg1.begin();k!=flg1.end();k++){
-    po[r][*k][num1[0]]=1; 
-    po[r][*k][num1[1]]=1; 
-	}
-    }
-    } 
-    if(flg2.size()==4){
-    if(flg2[0]==flg2[2] && flg2[1]==flg2[3]){    
-    cout<<"twin"<<endl;	
-	for(vector<int>::iterator k=flg2.begin();k!=flg2.end();k++)	{
-	for(int i=0;i<N;i++){
-		po[*k][r][i]=0;
-	}
-    }
-	for(vector<int>::iterator k=flg2.begin();k!=flg2.end();k++){
-    po[*k][r][num2[0]]=1; 
-    po[*k][r][num2[1]]=1; 
-	}
-    } 
-    }
-    flg1.clear();
-    flg2.clear();
-    num1.clear();
-    num2.clear();
-}
+
 itr++;
 }while(count!=0);
 
+printGrid(grid);             //passing parameters by address
+
+
+/*int *a=grid[0];                     //pointer stuff
 printGrid(grid); 
-cout<<count<<" "<<itr;
+cout<<count<<" "<<itr<<endl;
+cout<<grid+1<<endl<<grid<<endl;
+cout<<a+1<<endl;
+for(int i=0;i<N;i++){cout<<a+i<<endl;}*/
 
 
 } 
